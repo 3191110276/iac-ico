a663225826f55a517faac9785e089d76409ae6b8
Michael Maurer <3191110276@users.noreply.github.com>
2021-03-09 23:30:04 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    intersight = {
+      source = "CiscoDevNet/intersight"
+      version = "1.0.0"
+    }
+  }
+}
 
+provider "intersight" {
+  apikey = var.apikey
+  secretkey = var.secretkey
+  endpoint = "intersight.com"
+}
